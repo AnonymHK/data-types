dc917fa9f7383be88ae2626e443abcc657b68cbe
AnonymHK <reipay@foxmail.com>
2023-09-27 14:18:19 +0800

    update namespace

--- src/Buffer/Base64.php
+++ src/Buffer/Base64.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * This file is a part of "comely-io/data-types" package.
- * https://github.com/comely-io/data-types
+ * This file is a part of "anonymhk/data-types" package.
+ * https://github.com/anonymhk/data-types
  *
  * Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code or visit following link:
- * https://github.com/comely-io/data-types/blob/master/LICENSE
+ * https://github.com/anonymhk/data-types/blob/master/LICENSE
  */
 
 declare(strict_types=1);
